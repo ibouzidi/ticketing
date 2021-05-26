1c7d595ebeec27a596369bdd53d9e726590adc2d
idrisbouzidi42 <bouzidi.idris42@gmail.com>
2021-05-26 13:05:33 +0200

    modif bdd

--- ticketing.sql
+++ ticketing.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mer. 10 mars 2021 à 23:02
--- Version du serveur :  10.4.14-MariaDB
--- Version de PHP : 7.4.9
+-- Généré le : mer. 26 mai 2021 à 13:05
+-- Version du serveur :  10.4.11-MariaDB
+-- Version de PHP : 7.3.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -30,19 +31,16 @@ SET time_zone = "+00:00";
 CREATE TABLE `etats` (
   `id` int(11) NOT NULL,
   `nom_etat` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `etats`
 --
 
 INSERT INTO `etats` (`id`, `nom_etat`) VALUES
-(1, 'Ouvert'),
-(2, 'Fermé'),
 (3, 'En attente'),
-(4, 'Stand by'),
-(7, 'test'),
-(8, 'test2');
+(2, 'Fermer'),
+(1, 'Ouvert');
 
 -- --------------------------------------------------------
 
@@ -63,13 +61,14 @@ CREATE TABLE `t_commentaire` (
 --
 
 INSERT INTO `t_commentaire` (`COM_ID`, `COM_DATE`, `COM_AUTEUR`, `COM_CONTENU`, `tic_ID`) VALUES
-(3, '2020-11-06 10:38:14', 'fbd', 'fgerg', 3),
-(4, '2020-11-06 10:38:18', 'f', ' gfb', 3),
-(5, '2020-11-06 10:38:24', 'aa', 'aaaa', 3),
-(6, '2020-11-19 10:32:30', 'sc', 'sc', 11),
-(7, '2020-11-19 10:32:48', 'lkcdsjkvc', 'jwdk,vkds', 11),
-(8, '2020-11-30 15:39:43', 'idris', 'ahh walah sa se fait pas, tu abuse', 23),
-(9, '2020-11-30 15:40:43', 'yanis', 't vraiment un gros chien t\'aurais pu cacher les preuves\r\n', 23);
+(56, '2021-05-24 15:52:58', 'ergerg', 'ergergerger', 78),
+(57, '2021-05-24 15:53:01', 'erger', 'gergergergerg', 78),
+(58, '2021-05-24 15:53:03', 'ergergerg', 'erg', 78),
+(59, '2021-05-24 15:59:43', 'ezfezfefefefef', 'zefzefzefzefzefzefzeffezzefzefzefezfezfezfezfezfefzzefzefzefzefzefzeffzezfzefzefzefefz', 83),
+(60, '2021-05-24 15:59:46', 'zefzef', 'zefzef', 83),
+(61, '2021-05-24 15:59:49', 'zefzefz', 'efzefz', 83),
+(62, '2021-05-24 15:59:51', 'zfzef', 'zefzef', 83),
+(63, '2021-05-24 15:59:54', 'zefze', 'fzefzef', 83);
 
 -- --------------------------------------------------------
 
@@ -81,18 +80,19 @@ CREATE TABLE `t_ticket` (
   `TIC_ID` int(11) NOT NULL,
   `TIC_DATE` datetime NOT NULL,
   `TIC_TITRE` varchar(255) NOT NULL,
+  `TIC_AUTEUR` varchar(30) NOT NULL,
   `TIC_CONTENU` varchar(255) NOT NULL,
-  `TIC_ETAT` int(11) NOT NULL
+  `TIC_ETAT` int(11) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `t_ticket`
 --
 
-INSERT INTO `t_ticket` (`TIC_ID`, `TIC_DATE`, `TIC_TITRE`, `TIC_CONTENU`, `TIC_ETAT`) VALUES
-(1, '2020-03-20 16:26:48', 'Maintenance', 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley', 1),
-(3, '2020-12-16 15:07:56', 'Mise en place de FIX !', 'Mise en place de FIX !', 2),
-(25, '2020-12-16 15:07:50', 'Le popover ne bouge pas !!', 'Le popover ne bouge pas !!!', 4);
+INSERT INTO `t_ticket` (`TIC_ID`, `TIC_DATE`, `TIC_TITRE`, `TIC_AUTEUR`, `TIC_CONTENU`, `TIC_ETAT`) VALUES
+(194, '2021-05-26 13:04:13', 'Sed omnis officia ev', 'idris', 'Tempore qui et esse', 3),
+(195, '2021-05-26 13:04:06', 'A ea in vel eu corpo', 'Lellouche', 'Corporis pariatur S', 1),
+(196, '2021-05-26 13:03:30', 'Laboris sit porro a', 'Lucas', 'Dignissimos cupidita', 2);
 
 --
 -- Index pour les tables déchargées
@@ -102,7 +102,8 @@ INSERT INTO `t_ticket` (`TIC_ID`, `TIC_DATE`, `TIC_TITRE`, `TIC_CONTENU`, `TIC_E
 -- Index pour la table `etats`
 --
 ALTER TABLE `etats`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD UNIQUE KEY `nomEtatUnique` (`nom_etat`);
 
 --
 -- Index pour la table `t_commentaire`
@@ -115,7 +116,7 @@ ALTER TABLE `t_commentaire`
 --
 ALTER TABLE `t_ticket`
   ADD PRIMARY KEY (`TIC_ID`),
-  ADD KEY `TIC_ETAT` (`TIC_ETAT`);
+  ADD KEY `TIC_ETAT` (`TIC_ETAT`) USING BTREE;
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -125,19 +126,19 @@ ALTER TABLE `t_ticket`
 -- AUTO_INCREMENT pour la table `etats`
 --
 ALTER TABLE `etats`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- AUTO_INCREMENT pour la table `t_commentaire`
 --
 ALTER TABLE `t_commentaire`
-  MODIFY `COM_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `COM_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
 
 --
 -- AUTO_INCREMENT pour la table `t_ticket`
 --
 ALTER TABLE `t_ticket`
-  MODIFY `TIC_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `TIC_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=199;
 
 --
 -- Contraintes pour les tables déchargées
